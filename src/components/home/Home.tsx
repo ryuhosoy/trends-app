c391112b3644699317b3a53cac968626e4cba10c
RYUHEIPC\ryuho <longpingxigu@gmail.com>
2024-10-24 20:09:00 +0900

    ボタンの間隔を調整

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,12 +1,19 @@
 import { useNavigate } from "react-router-dom";
+import "./Home.css";
 
 function Home() {
   const navigate = useNavigate();
 
   return (
     <>
-      <button onClick={() => navigate("/RelatedQueries")}>関連トレンドを見る</button>
-      <button onClick={() => navigate("/DailyTrends")}>日常トレンドを見る</button>
+      <div className="HomeButtonWrp">
+        <button onClick={() => navigate("/RelatedQueries")}>
+          関連トレンドを見る
+        </button>
+        <button onClick={() => navigate("/DailyTrends")}>
+          日常トレンドを見る
+        </button>
+      </div>
     </>
   );
 }
